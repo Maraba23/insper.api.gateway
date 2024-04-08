32b53ac843f0407890cacd6379ffae6456953d08
Maraba23 <78489243+Maraba23@users.noreply.github.com>
2024-04-08 15:45:23 -0300

    arrumado yaml

--- src/main/java/insper/api/gateway/GatewayApplication.java
+++ src/main/java/insper/api/gateway/GatewayApplication.java
@@ -1,4 +1,4 @@
-package insper.store.gateway;
+package insper.api.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
